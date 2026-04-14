a118b91992bd5ee9438cae24b8b1c2443370b8cd
keilkim <kie009@naver.com>
2026-04-14 22:17:47 +0900

    Manual confirm on download done; install-to-homescreen banner + iOS/Android instructions modal (browser only)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shenzhen-v8';
+const CACHE = 'shenzhen-v9';
 const CORE = ['./', './index.html', './tiles.json', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
