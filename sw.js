a067b41f6978fd8617a40e5ed3bfff07c55813c8
keilkim <kie009@naver.com>
2026-04-14 22:00:22 +0900

    Animated gesture demos in onboarding (scroll/swipe/pinch/tap)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shenzhen-v6';
+const CACHE = 'shenzhen-v7';
 const CORE = ['./', './index.html', './tiles.json', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
