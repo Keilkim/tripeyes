15c0f3efbdf925a6b07650378e8040c1d66e500c
keilkim <kie009@naver.com>
2026-04-14 22:07:58 +0900

    Remove redundant in-hero swipe-hint (covered by onboarding)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shenzhen-v7';
+const CACHE = 'shenzhen-v8';
 const CORE = ['./', './index.html', './tiles.json', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
